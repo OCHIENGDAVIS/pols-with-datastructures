bf5c8c4c90d779da5dd859e02254f43e61b0b476
davis <davisochieng4@gmail.com>
2019-02-05 13:09:51 +0300

    [finishes #163675626] added the get all political parties endpoint

--- app/api/v1/utils.py
+++ app/api/v1/utils.py
@@ -1,6 +1,8 @@
 from app.api.v1.models.party_model import parties
 
+
 def validate_party_info(party_dict):
+    """A utility function to validate party inputs"""
     data = party_dict
     id = data.get("id", None)
     name = data.get('name', None)
@@ -9,27 +11,25 @@ def validate_party_info(party_dict):
     if id is None:
         return {"message": "id is required", "code": 400}
     elif not isinstance(id, int):
-        return {"message": "id must be a number", "code":400}
+        return {"message": "id must be a number", "code": 400}
     elif name is None:
-        return {"message": "name is required", "code":400}
+        return {"message": "name is required", "code": 400}
     elif len(name) < 1:
-        return {"message": "name can not be an empty string","code": 400}
-    elif not isinstance(name, str) :
-        return {"message": "name must be a string","code": 400}
+        return {"message": "name can not be an empty string", "code": 400}
+    elif not isinstance(name, str):
+        return {"message": "name must be a string", "code": 400}
     elif hqAddress is None:
-        return {"message": "hqAddress is required","code": 400}
+        return {"message": "hqAddress is required", "code": 400}
     elif len(hqAddress) < 1:
-        return {"message": "hqaddress  can not be an empty string","code": 400}
+        return {"message": "hqaddress  can not be an empty string", "code": 400}
     elif logoUrl is None:
-        return {"message": "party logo Url is required","code": 400}
+        return {"message": "party logo Url is required", "code": 400}
     elif len(logoUrl) < 1:
-        return {"message": "Logo url  can not be an empty string","code": 400}
-    elif not isinstance(logoUrl, str) :
-        return {"message": "logo must be a string","code": 400}
-    elif next(filter(lambda x:x['id']== id, parties), None):
-        return {"message": f"party wit id {id} already exists","code": 400}
-
-
+        return {"message": "Logo url  can not be an empty string", "code": 400}
+    elif not isinstance(logoUrl, str):
+        return {"message": "logo must be a string", "code": 400}
+    elif next(filter(lambda x: x['id'] == id, parties), None):
+        return {"message": "party with that id already exists", "code": 400}
 
 
 # def find_by_id(item_id, item_iterable ):
@@ -39,18 +39,16 @@ def validate_party_info(party_dict):
 #         return False
 
 def find_by_id(_id):
+    """A utility funtion to find a particular party by id"""
     for item in parties:
         if item['id'] == _id:
             return True
     return False
 
 
-def find_by_name(item_name, item_iterable ):
-    if next(filter(lambda x:x['name']== item_name, item_iterable), None):
+def find_by_name(item_name, item_iterable):
+    """A utility function to find a particular party by name"""
+    if next(filter(lambda x: x['name'] == item_name, item_iterable), None):
         return True
     else:
         return False
-
-
-
-
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+"""A  module to run the application"""
 from app.instance import config
 from app.main import create_app
 
@@ -5,6 +6,3 @@ from app.main import create_app
 if __name__ == '__main__':
     app = create_app(config)
     app.run()
-
-
-
