16dd11286fba5b808d4ca1eae251b15596dcf5c4
davis <davisochieng4@gmail.com>
2019-02-06 18:44:14 +0300

    [finishes #163761158] Add travis CI

--- app/api/v1/views/views.py
+++ app/api/v1/views/views.py
@@ -1,26 +1,26 @@
-from  flask import jsonify, request
+from flask import jsonify, request
 from flask import Blueprint
 from app.api.v1.models.party_model import Party, parties
 from app.api.v1.models.office_model import Office, offices
 from app.api.v1.utils import validate_party_info, find_item_by_id
 
 
-
 api = Blueprint('api', __name__)
 
 
-@api.route('/parties', methods = ['GET'])
+@api.route('/parties', methods=['GET'])
 def get_all_parties():
     all_parties = Party.get_all_parties()
     return jsonify(all_parties), 200
 
 
-@api.route('/parties', methods = ['POST'])
+@api.route('/parties', methods=['POST'])
 def create_a_party():
     data = request.get_json()
     validation_response = validate_party_info(data)
     if validation_response is None:
-        new_party = Party.create_party(data['id'], data['name'], data['hqAddress'], data['logoUrl'])
+        new_party = Party.create_party(
+            data['id'], data['name'], data['hqAddress'], data['logoUrl'])
         return jsonify({
             "status": 201,
             "data": [
@@ -31,11 +31,10 @@ def create_a_party():
             ]
         }), 201
     else:
-        return jsonify({"message":validation_response['message']}), validation_response['code']
+        return jsonify({"message": validation_response['message']}), validation_response['code']
 
 
-
-@api.route('/parties/<int:party_id>', methods = ['GET'])
+@api.route('/parties/<int:party_id>', methods=['GET'])
 def get_a_party(party_id):
     if not isinstance(party_id, int):
         return jsonify({"message": "id must be an integer"}), 400
@@ -45,10 +44,10 @@ def get_a_party(party_id):
         for party in parties:
             if party['id'] == party_id:
                 return jsonify(party), 200
-        return jsonify({"message":"party do not exists"}), 404
+        return jsonify({"message": "party do not exists"}), 404
 
 
-@api.route('/parties/<int:party_id>', methods = ['DELETE'])
+@api.route('/parties/<int:party_id>', methods=['DELETE'])
 def delete_a_party(party_id):
     if not isinstance(party_id, int):
         return jsonify({"message": "ID must be an integer"}), 400
@@ -68,22 +67,22 @@ def delete_a_party(party_id):
                     ]
                 }), 200
         return jsonify({
-            "status":404,
+            "status": 404,
             "data": [{
                 "message": "party does not exists"
             }]
         }), 404
 
 
-@api.route('/parties/<int:party_id>/name', methods = ['PATCH'])
+@api.route('/parties/<int:party_id>/name', methods=['PATCH'])
 def edit_a_party(party_id):
     data = request.get_json()
     name = data.get("name", None)
     if not isinstance(party_id, int):
-        return jsonify({"message":"ID must be an integer"}), 400
+        return jsonify({"message": "ID must be an integer"}), 400
     elif name is None:
         return jsonify({'message': 'You must provide the new  name'}), 400
-    elif len(data['name']) <0:
+    elif len(data['name']) < 0:
         return jsonify({"message": "name cannot be empty"})
     else:
         for party in parties:
@@ -101,7 +100,7 @@ def edit_a_party(party_id):
         return jsonify({"message": "Party does not exists"}), 404
 
 
-@api.route('/offices', methods = ['GET'])
+@api.route('/offices', methods=['GET'])
 def get_all_offices():
     data = []
     for office in offices:
@@ -117,12 +116,12 @@ def get_all_offices():
     }), 200
 
 
-@api.route('/offices', methods = ['POST'])
+@api.route('/offices', methods=['POST'])
 def create_an_office():
     data = request.get_json()
     office_id = data.get("id", None)
     office_type = data.get("type", None)
-    office_name  = data.get("name")
+    office_name = data.get("name")
     if office_id is None:
         return jsonify({"message": "Id is required"}), 400
     elif office_type is None:
@@ -135,15 +134,15 @@ def create_an_office():
         new_office = Office.create_office(office_id, office_type, office_name)
         return jsonify({
             "status": 201,
-            "data" : [{
-                "id":new_office["id"],
+            "data": [{
+                "id": new_office["id"],
                 "type": new_office["type"],
                 "name": new_office["name"]
-            }  ]
+            }]
         }), 201
 
 
-@api.route('/offices/<int:office_id>', methods = ['GET'])
+@api.route('/offices/<int:office_id>', methods=['GET'])
 def get_an_office(office_id):
     if not isinstance(office_id, int):
         return jsonify({
@@ -160,15 +159,15 @@ def get_an_office(office_id):
             if office["id"] == office_id:
                 return jsonify({
                     "status": 200,
-                    "data":[{
+                    "data": [{
                         "id": office["id"],
-                        "type" : office["type"],
+                        "type": office["type"],
                         "name": office["name"]
-                    } ]
-                }),200
+                    }]
+                }), 200
         return jsonify({
             "status": 404,
             "data": [{
-                "message":"Office do not exists"
+                "message": "Office do not exists"
             }]
         })
