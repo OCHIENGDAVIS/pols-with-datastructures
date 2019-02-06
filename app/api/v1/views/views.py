6479212215bcf23165ea181dcb026dda9a042a4a
davis <davisochieng4@gmail.com>
2019-02-06 07:19:03 +0300

    [finishes #163675661] added an endpoint to edit a particular political partys' name

--- app/api/v1/views/views.py
+++ app/api/v1/views/views.py
@@ -51,9 +51,31 @@ def get_a_party(party_id):
 def delete_a_party(id):
     pass
 
-@api.route('/parties/<int:id>', methods = ['PATCH'])
-def edit_a_party(id):
-    pass
+@api.route('/parties/<int:party_id>/name', methods = ['PATCH'])
+def edit_a_party(party_id):
+    data = request.get_json()
+    name = data.get("name", None)
+    if not isinstance(party_id, int):
+        return jsonify({"message":"ID must be an integer"}), 400
+    elif name is None:
+        return jsonify({'message': 'You must provide the new  name'}), 400
+    elif len(data['name']) <0:
+        return jsonify({"message": "name cannot be empty"})
+    else:
+        for party in parties:
+            if party['id'] == party_id:
+                party['name'] = data['name']
+                return jsonify({
+                    "status": 200,
+                    "data": [
+                        {
+                            "id": party['id'],
+                            "name": party['name']
+                        }
+                    ]
+                })
+        return jsonify({"message": "Party does not exists"}), 404
+
 
 @api.route('/offices', methods = ['GET'])
 def get_all_offices():
